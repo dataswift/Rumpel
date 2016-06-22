aadee6eaf0988981be9d15c73b136540e79af2ea
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2016-06-22 15:43:38 +0100

    Moves to RC3 release

--- src/app/rumpel.routes.ts
+++ src/app/rumpel.routes.ts
@@ -5,12 +5,12 @@ import { AuthComponent } from './auth';
 import { LocationsComponent, CalendarComponent, PhotosComponent, MixpadComponent } from './dataViews';
 
 export const routes: RouterConfig = [
-  { path: '/authenticate/:jwt', component: AuthComponent },
-  { path: '/dashboard', component: GridComponent },
-  { path: '/locations', component: LocationsComponent },
-  { path: '/calendar', component: CalendarComponent },
-  { path: '/photos', component: PhotosComponent },
-  { path: '/mixpad', component: MixpadComponent }
+  { path: '', component: GridComponent },
+  { path: 'authenticate/:jwt', component: AuthComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'mixpad', component: MixpadComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -11,17 +11,17 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 export class SideMenuComponent implements OnInit {
   public selectedItem: string;
   public menuItems = [
-    { display: 'Profile', icon: 'user', link: '/' },
-    { display: 'Offers', icon: 'tags', link: '/' },
-    { display: 'Messages', icon: 'chats', link: '/' },
-    { display: 'Locations', icon: 'tags', link: '/locations' },
-    { display: 'Calendar', icon: 'calendar', link: '/calendar' },
-    { display: 'Social', icon: 'replyall', link: '/' },
-    { display: 'Mail', icon: 'send', link: '/' },
-    { display: 'Weather', icon: 'partlycloudy', link: '/' },
-    { display: 'Photos', icon: 'camera', link: '/' },
-    { display: 'Plugins', icon: 'connection', link: '/' },
-    { display: 'Settings', icon: 'settings', link: '/' }
+    { display: 'Profile', icon: 'user', link: '' },
+    { display: 'Offers', icon: 'tags', link: '' },
+    { display: 'Messages', icon: 'chats', link: '' },
+    { display: 'Locations', icon: 'tags', link: 'locations' },
+    { display: 'Calendar', icon: 'calendar', link: 'calendar' },
+    { display: 'Social', icon: 'replyall', link: '' },
+    { display: 'Mail', icon: 'send', link: '' },
+    { display: 'Weather', icon: 'partlycloudy', link: '' },
+    { display: 'Photos', icon: 'camera', link: '' },
+    { display: 'Plugins', icon: 'connection', link: '' },
+    { display: 'Settings', icon: 'settings', link: '' }
   ];
 
   constructor() {}
