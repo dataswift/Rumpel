33c5e01f30ebce7687ee16c87bf2a37e52d223db
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2016-07-23 16:49:28 +0200

    Provides content links and styles to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'rump-footer',
   templateUrl: 'footer.component.html',
-  styleUrls: ['footer.component.css']
+  styleUrls: ['footer.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class FooterComponent implements OnInit {
 
