b4278a22c91863e094cd55b87f5bc9bddf238e04
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2016-07-25 21:39:28 +0100

    Fixes bug with data debit confirmation/cancellation

--- src/app/transactions/data-debit-confirm/data-debit-confirm.component.ts
+++ src/app/transactions/data-debit-confirm/data-debit-confirm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataDebitService, HatApiService } from '../../services';
 import { Moment } from '../../pipes/moment.pipe';
 
@@ -17,7 +17,8 @@ export class DataDebitConfirmComponent implements OnInit {
 
   constructor(private _route: ActivatedRoute,
               private _ddSvc: DataDebitService,
-              private _hat: HatApiService) {}
+              private _hat: HatApiService,
+              private router: Router) {}
 
   ngOnInit() {
     this.offerInfo = {
@@ -42,11 +43,11 @@ export class DataDebitConfirmComponent implements OnInit {
   }
 
   acceptDataDebit() {
-    this._hat.updateDataDebit(this._uuid, 'enable');
+    this._hat.updateDataDebit(this._uuid, 'enable').subscribe(res => this.router.navigate(['']));
   }
 
   rejectDataDebit() {
-    this._hat.updateDataDebit(this._uuid, 'disable');
+    this._hat.updateDataDebit(this._uuid, 'disable').subscribe(res => this.router.navigate(['']));
   }
 
 }
