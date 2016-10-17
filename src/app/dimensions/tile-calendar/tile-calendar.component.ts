06c1a3e78105cc123b207dce90d0209c8522f4a7
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2016-10-17 12:47:15 +0100

    Updates events dashboard tile to display upcoming event

--- src/app/dimensions/tile-calendar/tile-calendar.component.ts
+++ src/app/dimensions/tile-calendar/tile-calendar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventsService } from '../events.service';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'rump-tile-calendar',
   templateUrl: 'tile-calendar.component.html',
@@ -8,14 +10,29 @@ import { EventsService } from '../events.service';
 })
 export class TileCalendarComponent implements OnInit, OnDestroy {
   public events;
+  public upcomingEventsExist: boolean;
   private sub: any;
 
   constructor(private eventsSvc: EventsService) {}
 
   ngOnInit() {
+    this.events = [];
     this.sub = this.eventsSvc.getEvents$()
       .subscribe(events => {
-        this.events = events;
+        let upcomingEvents = events
+          .filter(event => event.timestamp.isAfter() && event.timestamp.isBefore(moment().add(1, 'days').endOf('day')))
+          .sort((a, b) => a.timestamp.isAfter(b.timestamp) ? 1 : -1);
+
+        if (upcomingEvents.length > 0) {
+          this.upcomingEventsExist = true;
+          let daySplitIndex = upcomingEvents.findIndex(event => event.timestamp.isAfter(moment().endOf('day')))
+          this.events = [
+            { relativeTime: 'today', events: upcomingEvents.splice(0, daySplitIndex) },
+            { relativeTime: 'tomorrow', events: upcomingEvents }
+          ];
+        } else {
+          this.upcomingEventsExist = false;
+        }
       });
     this.eventsSvc.showAll();
   }
