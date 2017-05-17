1b7bf1ff0bedb8834f5dffda2ccc8f4cf67baf44
Jon Hewines <jonhewines@users.noreply.github.com>
2017-05-17 20:57:01 +0100

    Adjusts location data pulled into mashup to improve performance

--- src/app/mashups/my-day/my-day.component.ts
+++ src/app/mashups/my-day/my-day.component.ts
@@ -115,7 +115,7 @@ export class MyDayComponent implements OnInit, OnDestroy {
 
       if (locations.length > this.totalDP) {
         this.totalDP = locations.length;
-        this.locationsSvc.getMoreData(500, 5000);
+        this.locationsSvc.getMoreData(100, 5000);
       }
     });
 
