355ea1444aa139fd860896afc77c8aa681eafd6e
Jon Hewines <jonhewines@users.noreply.github.com>
2017-06-12 17:43:17 +0100

    Reduces delay on sidebar popups

--- src/app/layout/side-menu/side-menu.component.ts
+++ src/app/layout/side-menu/side-menu.component.ts
@@ -145,7 +145,7 @@ export class SideMenuComponent implements OnInit {
 
 
   showPopover() {
-    $('[data-toggle="popover"]').popover();
+    $('.menu-scroll [data-toggle="popover"]').popover({delay: {show: 150, hide: 0}});
   }
 
   animateMenu() {
