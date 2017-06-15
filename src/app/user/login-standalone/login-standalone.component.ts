07fde16d86430490af881b5afe1c9845682c4d60
Jon Hewines <jonhewines@users.noreply.github.com>
2017-06-15 17:43:35 +0100

    Adds hubat.net to list of login domains

--- src/app/user/login-standalone/login-standalone.component.ts
+++ src/app/user/login-standalone/login-standalone.component.ts
@@ -12,7 +12,7 @@ export class LoginStandaloneComponent implements OnInit {
   public lastLoginId: string;
   private redirectPath: string;
   public dropdownExpanded = false;
-  private availableDomains: Array<string> = ['.hubofallthings.net', '.bheard.org'];
+  private availableDomains: Array<string> = ['.hubofallthings.net', '.hubat.net', '.bheard.org'];
   public selectedDomain: string;
 
   constructor(@Inject(APP_CONFIG) public config: IAppConfig,
