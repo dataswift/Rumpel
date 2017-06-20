53b6807ebd19bdfbecf41bbb7728c3fb548828f7
Jon Hewines <jonhewines@users.noreply.github.com>
2017-06-20 15:34:22 +0100

    Updates offers interface

--- src/app/offers/offer-modal/offer-modal.component.ts
+++ src/app/offers/offer-modal/offer-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Moment } from 'moment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'rump-offer-modal',
@@ -6,17 +8,39 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./offer-modal.component.scss']
 })
 export class OfferModalComponent implements OnInit {
-  @Input() title: string;
-  @Input() message: string;
+  @Input() offer_index: number;
+  @Input() offers: any;
+
+  @Input() changeModal: Function;
+
+  public timeNow = Date.now();
+
   private destroy: Function;
 
   constructor() { }
 
   ngOnInit() {
+    console.log( this.offers[this.offer_index] );
   }
 
   closeModal(): void {
     this.destroy();
+  }
+
+  prevOffer(): void {
+    if(this.offer_index > 0 ) {
+      this.offer_index = this.offer_index - 1;
+    }
+  }
+
+  nextOffer(): void {
+    if(this.offers.length > (this.offer_index + 1) ) {
+      this.offer_index = this.offer_index + 1;
+    }
+  }
+
+  acceptOffer(): void {
+    this.closeModal();
   }
 
 }
--- src/app/offers/offers-component/offers.component.ts
+++ src/app/offers/offers-component/offers.component.ts
@@ -15,9 +15,10 @@ import { OfferModalComponent } from '../offer-modal/offer-modal.component';
 
 export class OffersComponent implements OnInit {
 
-  public offers: any = [{title: 'batman', illustrationUrl: 'assets/icons/facebook-plug.svg', 'expires': 1527116400000},
-                        {title: 'robin', illustrationUrl: 'assets/icons/twitter-plug.svg', 'expires': 1497889035000},
-                      {title: 'robin', illustrationUrl: 'assets/icons/twitter-plug.svg', 'expires': 1327116400000}]
+
+  public offers: any = [{title: 'batman', illustrationUrl: 'assets/icons/facebook-plug.svg', 'expires': 1527116400000, shortDescription: 'hello jon', requiredDataDefinition: [{'source': 'Facebook', 'datasets': [{'name': 'photo', 'description': 'your photos'},{'name': 'photo', 'description': 'your photos'}]}]},
+                        {title: 'robin', illustrationUrl: 'assets/icons/twitter-plug.svg', 'expires': 1497889035000, shortDescription: 'hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon', requiredDataDefinition: [{'source': 'Facebook', 'datasets': [{'name': 'photo', 'description': 'your photos'},{'name': 'photo', 'description': 'your photos'}]}]},
+                      {title: 'robin', illustrationUrl: 'assets/icons/twitter-plug.svg', 'expires': 1327116400000, shortDescription: 'hello jon', requiredDataDefinition: [{'source': 'Facebook', 'datasets': [{'name': 'photo', 'description': 'your photos'},{'name': 'photo', 'description': 'your photos'}]}]},]
 
   public expiry: String = '';
   public timeNow: Number = Date.now();
@@ -25,17 +26,15 @@ export class OffersComponent implements OnInit {
   constructor(  private dialogSvc: DialogService ) { }
 
   ngOnInit() {
-
   }
 
 
-  showModal() {
+  showModal(offerIndex) {
     this.dialogSvc.createDialog<OfferModalComponent>(OfferModalComponent, {
-      title: 'Help',
-      message: `HATs are distributed systems and being private also means no one will know if you have a problem.<br><br>
-      If you have an issue with your HAT or this dashboard, please report it <a href="mailto:contact@HATDeX.org?subject=Support%20for%20`
-      + window.location.hostname + `">here</a>`
+      offer_index: offerIndex,
+      offers: this.offers
     });
   }
+
 
 }
