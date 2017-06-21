c6ef79159cf3563f1673516e91e84e78737345ff
Jon Hewines <jonhewines@users.noreply.github.com>
2017-06-21 16:37:45 +0100

    Adds default images to offers, if not set from endpoint

--- src/app/offers/offers-component/offers.component.ts
+++ src/app/offers/offers-component/offers.component.ts
@@ -22,14 +22,15 @@ export class OffersComponent implements OnInit {
   public offers: any = [
     {
       id: 'b25334ab-707a-42e7-9a38-6d1e65604602',
-      title: 'batman',
-      illustrationUrl: 'assets/icons/facebook-plug.svg',
+      title: '£1 hot drink voucher',
+      illustrationUrl: '',
       expires: 1527116400000,
       shortDescription: 'hello jon',
       pii: true,
       collectFor: 259200000,
       requiredMaxUser: 1000,
       totalUserClaims: 587,
+      reward: {rewardType: 'Cash'},
       requiredDataDefinition:
           [{
             'source': 'Facebook',
@@ -38,7 +39,7 @@ export class OffersComponent implements OnInit {
     },
     {
       id: '456',
-      title: 'robin',
+      title: 'Compare your health data to other people',
       illustrationUrl: 'assets/icons/twitter-plug.svg',
       expires: 1497889035000,
       shortDescription: 'hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon',
@@ -46,6 +47,7 @@ export class OffersComponent implements OnInit {
       collectFor: 691200000,
       requiredMaxUser: 100,
       totalUserClaims: 100,
+      reward: {rewardType: 'Voucher'},
       requiredDataDefinition:
           [{
             'source': 'Twitter',
@@ -53,14 +55,15 @@ export class OffersComponent implements OnInit {
           }]
     },
     {
-      id: '783', title: 'robin',
-      illustrationUrl: 'assets/icons/twitter-plug.svg',
+      id: '783', title: 'Share 1 day of your Fitbit data and earn £0.10p',
+      illustrationUrl: 'assets/icons/fitbit-plug.svg',
       expires: 1327116400000,
       shortDescription: 'hello jon',
       pii: true,
       collectFor: 86400000,
       requiredMaxUser: 10,
       totalUserClaims: 1,
+      reward: {rewardType: 'Service'},
       requiredDataDefinition:
           [{
             'source': 'Location',
@@ -80,6 +83,9 @@ export class OffersComponent implements OnInit {
                 private dataOfferSvc: DataOfferService) { }
 
   ngOnInit() {
+
+    const self = this;
+
     this.offersSub = this.dataOfferSvc.fetchUserAwareOfferList().subscribe(offers => {
       this.offers = offers.filter(function(offer) {
           return (  offer.claim.status === 'untouched'
@@ -87,9 +93,33 @@ export class OffersComponent implements OnInit {
                     && offer.expires > Date.now()
                   )
       });
+
+      self.setOfferImage();
     },
     error => { console.log(error); });
 
+    this.setOfferImage();
+  }
+
+
+  setOfferImage() {
+    for (let i = 0; i < this.offers.length; i++) {
+
+      if (this.offers[i].illustrationUrl === '') {
+        const offerType = this.offers[i].reward.rewardType.toLowerCase();
+
+        if ( offerType === 'cash' ) {
+          this.offers[i].illustrationUrl = 'assets/images/coin-icon.svg';
+        } else if ( offerType === 'voucher' ) {
+          this.offers[i].illustrationUrl = 'assets/images/voucher-icon.svg';
+        } else if ( offerType === 'service' ) {
+          this.offers[i].illustrationUrl = 'assets/images/badge-icon.svg';
+        }
+
+      }
+
+      console.log(this.offers[i].illustrationUrl);
+    }
   }
 
 
