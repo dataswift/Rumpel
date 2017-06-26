7265464716ae941100d95a737d58c0e8f972adcc
Jon Hewines <jonhewines@users.noreply.github.com>
2017-06-26 21:13:04 +0100

    Adds accepted offers component

--- src/app/offers/offers-accepted/offers-accepted.component.ts
+++ src/app/offers/offers-accepted/offers-accepted.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { DialogService } from '../../layout/dialog.service';
+import { OfferModalComponent } from '../offer-modal/offer-modal.component';
+import { InfoBoxComponent } from '../../layout/info-box/info-box.component';
+
 
 @Component({
   selector: 'rump-offers-accepted',
   templateUrl: './offers-accepted.component.html',
   styleUrls: ['./offers-accepted.component.scss']
 })
+
 export class OffersAcceptedComponent implements OnInit {
 
-  constructor() { }
+
+  @Input() offers: any = [];
+  @Input() acceptedOffers: any = [];
+  @Input() noOffersMessage = '';
+
+  public vouchersEarned = 0;
+  public vouchersClaimed = 0;
+
+  public servicesEarned = 0;
+  public servicesClaimed = 0;
+
+  public cashEarned = 0;
+  public cashClaimed = 0;
+  public cashFormat = '1.2-2';
+
+
+  constructor( private dialogSvc: DialogService ) { }
 
   ngOnInit() {
+
   }
+
+
+
+
+  showModal(offerIndex) {
+    this.dialogSvc.createDialog<OfferModalComponent>(OfferModalComponent, {
+      offer_index: offerIndex,
+      offers: this.acceptedOffers
+    });
+  }
+
+
 
 }
