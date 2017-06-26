b53ab7e59fba14a9c817169b69abee3dff22518f
Jon Hewines <jonhewines@users.noreply.github.com>
2017-06-26 21:12:46 +0100

    Updates Offer component to remove nav elements and sample data

--- src/app/offers/offers-component/offers.component.ts
+++ src/app/offers/offers-component/offers.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Moment } from 'moment';
-import * as moment from 'moment';
-import { Router } from '@angular/router';
-import {Subscription, Observable} from 'rxjs/Rx';
+import { Component, OnInit, Input } from '@angular/core';
 
-import { DataOfferService } from '../../data-management/data-offer.service';
 import { DialogService } from '../../layout/dialog.service';
 import { OfferModalComponent } from '../offer-modal/offer-modal.component';
+import { InfoBoxComponent } from '../../layout/info-box/info-box.component';
 
 @Component({
   selector: 'rump-offers',
@@ -17,109 +13,18 @@ import { OfferModalComponent } from '../offer-modal/offer-modal.component';
 
 export class OffersComponent implements OnInit {
 
-  private offersSub: Subscription;
 
-  public offers: any = [
-    {
-      id: 'b25334ab-707a-42e7-9a38-6d1e65604602',
-      title: '£1 hot drink voucher',
-      illustrationUrl: '',
-      expires: 1527116400000,
-      shortDescription: 'hello jon',
-      pii: true,
-      collectFor: 259200000,
-      requiredMaxUser: 1000,
-      totalUserClaims: 587,
-      reward: {rewardType: 'Cash'},
-      requiredDataDefinition:
-          [{
-            'source': 'Facebook',
-            'datasets': [{'name': 'photo', 'description': 'your photos'}, {'name': 'photo', 'description': 'your photos'}]
-          }]
-    },
-    {
-      id: '456',
-      title: 'Compare your health data to other people',
-      illustrationUrl: 'assets/icons/twitter-plug.svg',
-      expires: 1497889035000,
-      shortDescription: 'hello jon hello jon hello jon hello jon hello jon hello jon hello jon hello jon',
-      pii: false,
-      collectFor: 691200000,
-      requiredMaxUser: 100,
-      totalUserClaims: 100,
-      reward: {rewardType: 'Voucher'},
-      requiredDataDefinition:
-          [{
-            'source': 'Twitter',
-            'datasets': [{'name': 'title', 'description': 'body'}, {'name': 'photo', 'description': 'your photos'}]
-          }]
-    },
-    {
-      id: '783', title: 'Share 1 day of your Fitbit data and earn £0.10p',
-      illustrationUrl: 'assets/icons/fitbit-plug.svg',
-      expires: 1327116400000,
-      shortDescription: 'hello jon',
-      pii: true,
-      collectFor: 86400000,
-      requiredMaxUser: 10,
-      totalUserClaims: 1,
-      reward: {rewardType: 'Service'},
-      requiredDataDefinition:
-          [{
-            'source': 'Location',
-            'datasets': [{'name': 'photo', 'description': 'your photos'}, {'name': 'photo', 'description': 'your photos'}]
-          },
-          {
-            'source': 'Facebook',
-            'datasets': [{'name': 'photo', 'description': 'your photos'}, {'name': 'photo', 'description': 'your photos'}]
-          }]
-    }
-  ];
+  @Input() offers: any = [];
+  @Input() acceptedOffers: any = [];
+  @Input() noOffersMessage = '';
 
   public expiry: String = '';
   public timeNow: Number = Date.now();
 
-  constructor(  private dialogSvc: DialogService,
-                private dataOfferSvc: DataOfferService) { }
+  constructor(  private dialogSvc: DialogService ) { }
 
   ngOnInit() {
 
-    const self = this;
-
-    this.offersSub = this.dataOfferSvc.fetchUserAwareOfferList().subscribe(offers => {
-      this.offers = offers.filter(function(offer) {
-          return (  offer.claim.status === 'untouched'
-                    && (offer.requiredMaxUser - offer.totalUserClaims) > 0
-                    && offer.expires > Date.now()
-                  )
-      });
-
-      self.setOfferImage();
-    },
-    error => { console.log(error); });
-
-    this.setOfferImage();
-  }
-
-
-  setOfferImage() {
-    for (let i = 0; i < this.offers.length; i++) {
-
-      if (this.offers[i].illustrationUrl === '') {
-        const offerType = this.offers[i].reward.rewardType.toLowerCase();
-
-        if ( offerType === 'cash' ) {
-          this.offers[i].illustrationUrl = 'assets/images/coin-icon.svg';
-        } else if ( offerType === 'voucher' ) {
-          this.offers[i].illustrationUrl = 'assets/images/voucher-icon.svg';
-        } else if ( offerType === 'service' ) {
-          this.offers[i].illustrationUrl = 'assets/images/badge-icon.svg';
-        }
-
-      }
-
-      console.log(this.offers[i].illustrationUrl);
-    }
   }
 
 
@@ -127,6 +32,14 @@ export class OffersComponent implements OnInit {
     this.dialogSvc.createDialog<OfferModalComponent>(OfferModalComponent, {
       offer_index: offerIndex,
       offers: this.offers
+    });
+  }
+
+  showInfobox() {
+    this.dialogSvc.createDialog<InfoBoxComponent>(InfoBoxComponent, {
+      title: 'Trade your data for cash, services or vouchers',
+      message: `The offers you see on this page are tailored to you, based on the data you have chosen to share.<br><br>
+      You can unlock more offers by connecting more data plugs and entering your personal data.`
     });
   }
 
