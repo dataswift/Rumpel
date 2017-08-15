c29ff1b4e1bf8b6f5bec8b93bb2aae2c71bbec84
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2017-08-15 13:29:48 +0100

    Updates deployment configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -35,7 +35,7 @@ export class IAppConfig {
 
 export const AppConfig: IAppConfig = {
   version: '3.1.0',
-  name: 'RumpelStaging',
+  name: 'Rumpel',
   native: environment.native,
   protocol: environment.protocol,
   exchange: {
@@ -54,7 +54,7 @@ export const AppConfig: IAppConfig = {
   },
   databuyer: {
     name: 'DataBuyer',
-    url: 'https://databuyer.hubat.net'
+    url: 'https://databuyer.hubofallthings.com'
   },
   facebook: {
     shareUrl: 'https://www.facebook.com/sharer/sharer.php?u='
