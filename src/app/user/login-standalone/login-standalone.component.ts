49ce3cd3d5d3a5fd993b247faa96bdf94f0c7570
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2017-11-13 19:08:10 +0200

    Updates branding and appearance of the login views

--- src/app/user/login-standalone/login-standalone.component.ts
+++ src/app/user/login-standalone/login-standalone.component.ts
@@ -12,7 +12,7 @@ export class LoginStandaloneComponent implements OnInit {
   public lastLoginId: string;
   private redirectPath: string;
   public dropdownExpanded = false;
-  private availableDomains: Array<string> = ['.hubofallthings.net', '.hubat.net', '.savy.io'];
+  public availableDomains: Array<string> = ['.hubofallthings.net', '.hubat.net', '.savy.io'];
   public selectedDomain: string;
 
   constructor(@Inject(APP_CONFIG) public config: IAppConfig,
