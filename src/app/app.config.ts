a6a9da2f5b581a9a1af36e3f5cbec0f2a3d2c6c6
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2018-03-22 10:29:25 +0000

    Asks user to grant permissions when the app needs updating

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -33,7 +33,7 @@ export class AppConfig {
 }
 
 export const configuration: AppConfig = {
-  version: '3.3.2',
+  version: '3.4.1',
   name: 'RumpelStaging',
   supportedDomains: ['.hubat.net', '.hat.direct'],
   native: environment.native,
