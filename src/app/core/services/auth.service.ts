f1af3c9c2f751055cf80670f507312faa569ffe7
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2018-05-24 15:32:01 +0100

    Stops using Array.prototype.includes() in the codebase for compatibility

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -145,7 +145,7 @@ export class AuthService {
 
     const scopeIsValid = decodedToken['application'] === this.config.tokenApp || decodedToken['accessScope'] === 'owner';
     const tokenDomain = decodedToken['iss'].slice(decodedToken['iss'].indexOf('.'));
-    const domainIsValid = this.config.supportedDomains.includes(tokenDomain) || /^[\w.]+:9000$/.test(tokenDomain);
+    const domainIsValid = this.config.supportedDomains.indexOf(tokenDomain) > -1 || /^[\w.]+:9000$/.test(tokenDomain);
     const notExpired = isFuture(expiryDate) && isFuture(addDays(issuedDate, this.config.tokenExpiryTime));
 
     return scopeIsValid && domainIsValid && notExpired;
