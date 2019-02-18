5fdfaecf159a71c18f460e5b68ce4ce64eb5ab1a
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2019-02-18 13:22:10 +0200

    Fixes lint issue

--- src/app/hat-claim/hat-claim/hat-claim.component.spec.ts
+++ src/app/hat-claim/hat-claim/hat-claim.component.spec.ts
@@ -1,18 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HatClaimComponent } from './hat-claim.component';
-import {ActivatedRoute, Router} from '@angular/router';
-import {CustomAngularMaterialModule} from '../../core/custom-angular-material.module';
-import {RouterTestingModule} from '@angular/router/testing';
-import {FormsModule} from '@angular/forms';
-import {HatClaimSubscriptionsComponent} from '../hat-claim-subscriptions/hat-claim-subscriptions.component';
-import {HatClaimDetailsComponent} from '../hat-claim-details/hat-claim-details.component';
-import {HatClaimNewPasswordComponent} from '../hat-claim-new-password/hat-claim-new-password.component';
-import {HatClaimUrlComponent} from '../hat-claim-url/hat-claim-url.component';
-import {HatClaimConfirmationComponent} from '../hat-claim-confirmation/hat-claim-confirmation.component';
-import {HatClaimSuccessComponent} from '../hat-claim-success/hat-claim-success.component';
-import {HatClaimService} from '../hat-claim.service';
-import { of } from "rxjs";
+import { ActivatedRoute, Router } from '@angular/router';
+import { CustomAngularMaterialModule } from '../../core/custom-angular-material.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HatClaimSubscriptionsComponent } from '../hat-claim-subscriptions/hat-claim-subscriptions.component';
+import { HatClaimDetailsComponent } from '../hat-claim-details/hat-claim-details.component';
+import { HatClaimNewPasswordComponent } from '../hat-claim-new-password/hat-claim-new-password.component';
+import { HatClaimUrlComponent } from '../hat-claim-url/hat-claim-url.component';
+import { HatClaimConfirmationComponent } from '../hat-claim-confirmation/hat-claim-confirmation.component';
+import { HatClaimSuccessComponent } from '../hat-claim-success/hat-claim-success.component';
+import { HatClaimService } from '../hat-claim.service';
+import { of } from 'rxjs';
 
 describe('HatClaimComponent', () => {
   let component: HatClaimComponent;
