e88d6ff0d397d29844d647b38c86be753a9e3cc6
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2019-03-01 17:32:43 +0000

    Updates app configuration and bumps version number

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -34,10 +34,10 @@ export class AppConfig {
 
 export const configuration: AppConfig = {
   version: '3.7.0.1',
-  name: 'RumpelStaging',
-  tokenApp: 'hatappstaging',
+  name: 'Rumpel',
+  tokenApp: 'hatapp',
   tokenExpiryTime: 3,
-  supportedDomains: ['.hubat.net', '.hat.direct'],
+  supportedDomains: ['.hubofallthings.net', '.hat.direct'],
   native: environment.native,
   protocol: environment.protocol,
   dex: {
