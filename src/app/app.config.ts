fd0d3b60eae16c4f56258f91abea76c51c4d343e
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2019-04-17 15:37:50 +0100

    Updates app config for production environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -36,10 +36,10 @@ export class AppConfig {
 
 export const configuration: AppConfig = {
   version: '3.7.2.2',
-  name: 'RumpelStaging',
-  tokenApp: 'hatappstaging',
+  name: 'Rumpel',
+  tokenApp: 'hatapp',
   tokenExpiryTime: 3,
-  supportedDomains: ['.hubat.net', '.hat.direct'],
+  supportedDomains: ['.hubofallthings.net', '.hat.direct'],
   native: environment.native,
   protocol: environment.protocol,
   dex: {
